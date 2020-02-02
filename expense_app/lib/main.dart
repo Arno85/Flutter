57184a46ff80aad3f1314969dfd75b6e4d4c5e30
Arnaud <arnaud.martin85@gmail.com>
2020-02-02 13:29:29 -0500

    clean up main dart

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -44,16 +44,17 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  // String titleInput;
-  // String amountInput;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+
+  //#region Properties
+
   bool _showChart = false;
 
-  final List<Transaction> _transactions = [];
+  List<Transaction> _transactions = [];
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tx) {
@@ -64,6 +65,10 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }).toList();
   }
+
+  //#endregion
+
+  //#region Methods
 
   void _startAddNewTransaction(BuildContext ctx) {
     showModalBottomSheet(
@@ -95,11 +100,16 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final PreferredSizeWidget appBar = Platform.isIOS
+  //#endregion
+
+  //#region Widget Builder Methods
+
+  Widget _buildAppBar() {
+    final appTitle = 'Personal Expenses';
+
+    return Platform.isIOS
         ? CupertinoNavigationBar(
-            middle: Text('Personal Expenses'),
+            middle: Text(appTitle),
             trailing: Row(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
@@ -111,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           )
         : AppBar(
-            title: Text('Personal Expenses'),
+            title: Text(appTitle),
             actions: <Widget>[
               IconButton(
                 icon: Icon(Icons.add),
@@ -119,78 +129,94 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ],
           );
+  }
+
+  Widget _buildChartContent(double availableHeight, double screenSpacePct) {
+    return Container(
+              height: availableHeight * screenSpacePct,
+              child: Chart(_transactions),
+            );
+  }
+
+  Widget _buildTransactionsListContent(double availableHeight, double screenSpacePct) {
+    return Container(
+              height: availableHeight * screenSpacePct,
+              child: TransactionList(
+                _recentTransactions,
+                _deleteTransaction,
+              ),
+            );
+  }
+
+  List<Widget> _buildLandscapeContent(double availableHeight) {
+    return [
+      Container(
+        height: availableHeight * 0.15,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Show Shart',
+              style: Theme.of(context).textTheme.title,
+            ),
+            Switch.adaptive(
+              activeColor: Theme.of(context).accentColor,
+              value: _showChart,
+              onChanged: (val) {
+                setState(() => _showChart = val);
+              },
+            ),
+          ],
+        ),
+      ),
+      _showChart
+          ? _buildChartContent(availableHeight, 0.85)
+          : _buildTransactionsListContent(availableHeight, 0.85),
+    ];
+  }
+
+  List<Widget> _buildPortraitContent(double availableHeight) {
+    return [
+      _buildChartContent(availableHeight, 0.3),
+      _buildTransactionsListContent(availableHeight, 0.7)
+    ];
+  }
+
+  Widget _buildBody(bool isLandscape, double availableHeight) {
+   return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            if (isLandscape) ..._buildLandscapeContent(availableHeight),
+            if (!isLandscape) ..._buildPortraitContent(availableHeight)
+          ],
+        ),
+      ),
+    );
+  }
+
+  //#endregion
+
+  @override
+  Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
+    final PreferredSizeWidget appBar = _buildAppBar();
 
     final availableHeight = MediaQuery.of(context).size.height -
         appBar.preferredSize.height -
         MediaQuery.of(context).padding.top;
 
-    final isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
-
-    final body = SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            if (isLandscape)
-              Container(
-                height: availableHeight * 0.15,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Show Shart',
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    Switch.adaptive(
-                      activeColor: Theme.of(context).accentColor,
-                      value: _showChart,
-                      onChanged: (val) {
-                        setState(() => _showChart = val);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            if (isLandscape)
-              _showChart
-                  ? Container(
-                      height: availableHeight * 0.6,
-                      child: Chart(_transactions),
-                    )
-                  : Container(
-                      height: availableHeight * 0.85,
-                      child: TransactionList(
-                        _recentTransactions,
-                        _deleteTransaction,
-                      ),
-                    ),
-            if (!isLandscape)
-              Container(
-                height: availableHeight * 0.3,
-                child: Chart(_transactions),
-              ),
-            if (!isLandscape)
-              Container(
-                height: availableHeight * 0.7,
-                child: TransactionList(
-                  _recentTransactions,
-                  _deleteTransaction,
-                ),
-              ),
-          ],
-        ),
-      ),
-    );
-
     return Platform.isIOS
         ? CupertinoPageScaffold(
-            child: body,
+            child: _buildBody(isLandscape, availableHeight),
             navigationBar: appBar,
           )
         : Scaffold(
             appBar: appBar,
-            body: body,
+            body: _buildBody(isLandscape, availableHeight),
             floatingActionButtonLocation:
                 FloatingActionButtonLocation.centerFloat,
             floatingActionButton: Platform.isIOS
